030da58c68450271e6ac57ab2a4b54a66f874f1d
Rainer Kuhn <ticktricktrack@gmail.com>
2026-03-22 18:20:47 +0000

    turbo hotwire

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -2,4 +2,43 @@ class QuestionsController < ApplicationController
   def index
     @questions = Current.user.questions
   end
+
+  def new
+    @question = Current.user.questions.new
+  end
+
+  def create
+    @question = Current.user.questions.new(question_params)
+
+    respond_to do |format|
+      if @question.save
+        format.turbo_stream
+        format.html { redirect_to root_path }
+      else
+        format.turbo_stream { render turbo_stream: turbo_stream.replace("#{helpers.dom_id(@question)}_form", partial: "form", locals: { question: @question }) }
+        format.html { render :new, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @question.update(question_params)
+        format.turbo_stream { render turbo_stream: turbo_stream.replace("#{helpers.dom_id(question)}", partial: "question", locals: { question: @question }) }
+        format.html { redirect_to root_path}
+      else
+        format.html { render :edit }
+      end
+    end
+  end
+
+  private
+
+  def set_question
+    @question = Current.user.questions.find(params[:id])
+  end
+
+  def question_params
+    params.require(:question).permit(:body, :title, :category, :status)
+  end
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,3 +1,7 @@
 class Question < ApplicationRecord
   belongs_to :user
+
+  # Broadcasters
+  after_create_commit -> { broadcast_append_to "questions", target: 'questions-list'}
+  after_update_commit -> { broadcast_replace_to "todos" }
 end
